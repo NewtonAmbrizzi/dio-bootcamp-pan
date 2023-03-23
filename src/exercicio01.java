316517a862e174deb672e97d242f61d7010b704a
NewtonAmbrizzi <newton.ambrizzi@gmail.com>
2023-03-22 22:35:09 -0300

    First commit

---
+++ src/exercicio01.java
@@ -0,0 +1,18 @@
+/*
+Faça um programa que leia conjuntos de dois valores, o primeiro representando o nome e o segundo representando a idade.
+(Pare o programa inserindo o valor 0 no campo nome)
+*/
+
+public class exercicio01 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    private static void lerNome() {
+        
+
+
+    }
+
+}
