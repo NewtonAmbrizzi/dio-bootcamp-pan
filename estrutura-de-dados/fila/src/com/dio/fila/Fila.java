9d5e6895f6eedee49e67af41be66e768aa478b72
NewtonAmbrizzi <newton.ambrizzi@gmail.com>
2023-03-26 10:35:37 -0300

    Utilizando a classe Fila

--- estrutura-de-dados/fila/src/com/dio/fila/Fila.java
+++ estrutura-de-dados/fila/src/com/dio/fila/Fila.java
@@ -27,6 +27,7 @@ public class Fila {
                     break;
                 }
             }
+            return primeiroNo;
         }
         return null;
     }
--- estrutura-de-dados/fila/src/com/dio/fila/Main.java
+++ estrutura-de-dados/fila/src/com/dio/fila/Main.java
@@ -9,7 +9,19 @@ public class Main {
         minhaFila.enqueue(new No("segundo"));
         minhaFila.enqueue(new No("terceiro"));
         minhaFila.enqueue(new No("quarto"));
+        
+        System.out.println(minhaFila);
+        
+        System.out.println(minhaFila.dequeue());
+        
+        System.out.println(minhaFila);
+        
+        minhaFila.enqueue(new No("ultimo"));
 
+        System.out.println(minhaFila);
+        
+        System.out.println(minhaFila.first());
+        
         System.out.println(minhaFila);
 
     }
