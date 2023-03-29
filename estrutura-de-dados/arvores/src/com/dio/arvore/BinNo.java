5ba65b851446aba2f5a78bf34d6e34e5f3a5a67c
NewtonAmbrizzi <newton.ambrizzi@gmail.com>
2023-03-29 01:02:42 -0300

    BinNo finalizado

--- estrutura-de-dados/arvores/src/com/dio/arvore/BinNo.java
+++ estrutura-de-dados/arvores/src/com/dio/arvore/BinNo.java
@@ -6,12 +6,42 @@ public class BinNo<T extends Comparable<T>> {
     private BinNo<T> noEsq;
     private BinNo<T> noDir;
 
+    public BinNo() {
+    }
+
     public BinNo(T conteudo) {
         this.conteudo = conteudo;
         this.noEsq = null;
         this.noDir = null;
     }
 
-    
-    
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public BinNo<T> getNoEsq() {
+        return noEsq;
+    }
+
+    public void setNoEsq(BinNo<T> noEsq) {
+        this.noEsq = noEsq;
+    }
+
+    public BinNo<T> getNoDir() {
+        return noDir;
+    }
+
+    public void setNoDir(BinNo<T> noDir) {
+        this.noDir = noDir;
+    }
+
+    @Override
+    public String toString() {
+        return "BinNo [conteudo=" + conteudo + "]";
+    }
+
 }
